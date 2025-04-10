ea6e7b27b2b3fb99202f6f14c1b3d0ac5c4cc83e
NeeMiyuki <zaxarov2706@gmail.com>
2025-04-10 21:26:34 +0300

    Update ops_omp.hpp

--- tasks/omp/shulpin_i_jarvis_passage/include/ops_omp.hpp
+++ tasks/omp/shulpin_i_jarvis_passage/include/ops_omp.hpp
@@ -16,24 +16,12 @@ struct Point {
   Point(double x_coordinate, double y_coordinate) : x(x_coordinate), y(y_coordinate) {}
 };
 
-struct PointHash {
-  size_t operator()(const Point& p) const {
-    size_t hx = std::hash<double>{}(p.x);
-    size_t hy = std::hash<double>{}(p.y);
-    return hx ^ (hy << 1);
-  }
+struct PointEqual {
+  bool operator()(const Point& a, const Point& b) const { return a.x == b.x && a.y == b.y; }
 };
 
 struct PointEqual {
-  bool operator()(const Point& a, const Point& b) const {
-    return a.x == b.x && a.y == b.y;
-  }
-};
-
-struct PointEqual {
-  bool operator()(const Point& a, const Point& b) const {
-    return a.x == b.x && a.y == b.y;
-  }
+  bool operator()(const Point& a, const Point& b) const { return a.x == b.x && a.y == b.y; }
 };
 
 class JarvisSequential : public ppc::core::Task {
